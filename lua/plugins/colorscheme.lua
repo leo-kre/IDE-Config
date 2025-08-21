5e2ef41115f0c7922aaa40dc8f6e73b62dfee981
Leon Kreutzer <lfk.kre@gmail.com>
2025-08-21 12:12:59 +0200

    updating theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,52 +1,14 @@
--- lua/plugins/colorscheme.lua
 return {
   {
-    "deparr/tairiki.nvim",
+    "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-      palette = "dark",
-      default_dark = "dark",
-      default_light = "light",
-      transparent = false,
-      terminal = false,
-      end_of_buffer = false,
-      visual_bold = false,
-      cmp_itemkind_reverse = false,
-      diagnostics = {
-        darker = false,
-        background = true,
-        undercurl = false,
-      },
-      code_style = {
-        comments = { italic = true },
-        conditionals = {},
-        keywords = {},
-        functions = {},
-        strings = {},
-        variables = {},
-        parameters = {},
-        types = {},
-      },
-      lualine = { transparent = true },
-      plugins = {
-        all = false,
-        none = false,
-        auto = false,
-        treesitter = true,
-        semantic_tokens = true,
-      },
-      colors = function(colors, opts) end,
-      highlights = function(groups, colors, opts) end,
-    },
-    config = function(_, opts)
-      require("tairiki").setup(opts)
+    config = function()
+      require("kanagawa").setup({
+        theme = "dragon",
+      })
+      vim.cmd("colorscheme kanagawa-dragon")
     end,
   },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tairiki",
-    },
-  },
+  { "LazyVim/LazyVim", opts = { colorscheme = "kanagawa-dragon" } },
 }
